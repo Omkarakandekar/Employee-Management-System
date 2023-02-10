3f94921b5ea472ccf7455f897f6bffea5bd5ed4d
Omkar Kandekar <94789138+Omkarakandekar@users.noreply.github.com>
2023-02-10 20:57:44 +0530

    Add files via upload

---
+++ Employee Management System/src/employee/management/system/Conn.java
@@ -0,0 +1,21 @@
+package employee.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem", "root", "Omkar@8298");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+           
+            
+        }
+    }
+}
---
+++ Employee Management System/src/employee/management/system/Home.java
@@ -0,0 +1,72 @@
+package employee.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Home extends JFrame implements ActionListener{
+
+    JButton view, add, update, remove;
+    
+    Home() {
+        
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("NEW ICONS/Home.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1120, 630, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 1120, 630);
+        add(image);
+        
+        JLabel heading = new JLabel("Employee Management System");
+        heading.setBounds(140, 40, 450, 40);
+        heading.setFont(new Font("Raleway", Font.BOLD, 25));
+        heading.setForeground(Color.WHITE);
+        image.add(heading);
+        
+        add = new JButton("Add Employee");
+        add.setBounds(150, 100, 150, 40);
+        add.addActionListener(this);
+        image.add(add);
+        
+        view = new JButton("View Employees");
+        view.setBounds(350, 100, 150, 40);
+        view.addActionListener(this);
+        image.add(view);
+        
+        update = new JButton("Update Employee");
+        update.setBounds(150, 160, 150, 40);
+        update.addActionListener(this);
+        image.add(update);
+        
+        remove = new JButton("Remove Employee");
+        remove.setBounds(350, 160, 150, 40);
+        remove.addActionListener(this);
+        image.add(remove);
+        
+        setSize(1120, 630);
+        setLocation(250, 100);
+        setVisible(true);
+    }
+     public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == add) {
+            setVisible(false);
+            new AddEmployee();
+        } else if (ae.getSource() == view) {
+            setVisible(false);
+            new ViewEmployee();
+        } else if (ae.getSource() == update) {
+            setVisible(false);
+            new updateEmployee();
+        } else {
+            setVisible(false);
+            new RemoveEmployee();
+        }
+    }
+
+   
+    public static void main(String[] args) {
+        new Home();
+    }
+}
